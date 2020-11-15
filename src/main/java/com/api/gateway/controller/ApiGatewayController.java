2c2fddddc76a0e34841f858889ead4c4ac61ec30
ZakEnn <ennaje.zakari@gmail.com>
2020-11-15 23:45:30 +0100

    test

--- src/main/java/com/api/gateway/controller/ApiGatewayController.java
+++ src/main/java/com/api/gateway/controller/ApiGatewayController.java
@@ -1,8 +1,22 @@
 package com.api.gateway.controller;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiGatewayController {
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+						// Simple re-route from: /get to: http://httpbin.org/80
+						// And adds a simple "hello:world" HTTP Header
+						.route(p -> p
+										.path("/get")
+										.filters(f -> f.addRequestHeader("Hello", "World"))
+										.uri("http://httpbin.org:80"))
+						.build();
+	}
 }
