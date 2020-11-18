96fff1ea6b96a19dd07cc4182dc8b8a50bb0d672
ZakEnn <ennaje.zakari@gmail.com>
2020-11-18 19:51:20 +0100

    adding keycloak configuration

--- src/main/java/com/api/gateway/controller/ApiGatewayController.java
+++ src/main/java/com/api/gateway/controller/ApiGatewayController.java
@@ -3,6 +3,7 @@ package com.api.gateway.controller;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,12 +12,15 @@ public class ApiGatewayController {
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
-						// Simple re-route from: /get to: http://httpbin.org/80
-						// And adds a simple "hello:world" HTTP Header
-						.route(p -> p
-										.path("/get")
-										.filters(f -> f.addRequestHeader("Hello", "World"))
-										.uri("http://httpbin.org:80"))
+						.route(p -> p.path("/httpbin/**")
+									 .filters(f -> f.addRequestHeader("Hello", "World"))
+									 .uri("http://httpbin.org:80"))
+						
+						.route(p -> p.path("/httpbin/cookies")
+									.and()
+									.method(HttpMethod.GET)
+									.filters(f -> f.setPath("/cookies"))
+									.uri("http://httpbin.org:80"))
 						.build();
 	}
 }
