d1254c0c175f67e12bd06e357df61e4d9651502d
zakenn <ennaje.zakari@gmail.com>
2020-11-22 23:47:28 +0100

    refactoring

--- src/main/java/com/api/gateway/DemoApplication.java
+++ src/main/java/com/api/gateway/DemoApplication.java
@@ -2,6 +2,10 @@ package com.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +13,19 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+						.route(p -> p.path("/httpbin/**")
+									 .filters(f -> f.addRequestHeader("Hello", "World"))
+									 .uri("http://httpbin.org:80"))
+						
+						.route(p -> p.path("/httpbin/cookies")
+									.and()
+									.method(HttpMethod.GET)
+									.filters(f -> f.setPath("/cookies"))
+									.uri("http://httpbin.org:80"))
+						.build();
+	}
 }
--- src/main/java/com/api/gateway/controller/ApiGatewayController.java
+++ src/main/java/com/api/gateway/controller/ApiGatewayController.java
@@ -1,26 +1,9 @@
 package com.api.gateway.controller;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiGatewayController {
 
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-						.route(p -> p.path("/httpbin/**")
-									 .filters(f -> f.addRequestHeader("Hello", "World"))
-									 .uri("http://httpbin.org:80"))
-						
-						.route(p -> p.path("/httpbin/cookies")
-									.and()
-									.method(HttpMethod.GET)
-									.filters(f -> f.setPath("/cookies"))
-									.uri("http://httpbin.org:80"))
-						.build();
-	}
+	
 }
