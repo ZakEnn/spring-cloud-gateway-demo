dc9edf20a3d49c46a2f160e62efede6f468117c9
zakenn <ennaje.zakari@gmail.com>
2020-11-27 23:54:57 +0100

    test lb and tokenrelay

--- src/main/java/com/api/gateway/DemoApplication.java
+++ src/main/java/com/api/gateway/DemoApplication.java
@@ -1,14 +1,18 @@
 package com.api.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 
 @SpringBootApplication
 public class DemoApplication {
+	@Autowired
+	private TokenRelayGatewayFilterFactory filterFactory;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -26,6 +30,17 @@ public class DemoApplication {
 									.method(HttpMethod.GET)
 									.filters(f -> f.setPath("/cookies"))
 									.uri("http://httpbin.org:80"))
+						
+						.route("resource", r -> r.path("/resource")
+								.filters(f -> f.filters(filterFactory.apply())
+												.removeRequestHeader("Cookie")) // Prevents cookie being sent downstream
+								.uri("http://resource:9000")) // Taking advantage of docker naming
+						
+						.route("hello", ps-> {
+							return ps.path("/hello")
+									.uri("lb://helloservice");
+						})
+						
 						.build();
 	}
 }
