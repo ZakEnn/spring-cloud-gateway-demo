570833f70f649ed9c5fb632dc015e37bc5bc9c1c
zakenn <ennaje.zakari@gmail.com>
2020-12-05 23:58:18 +0100

    refactoring

--- src/main/java/com/api/gateway/DemoApplication.java
+++ src/main/java/com/api/gateway/DemoApplication.java
@@ -1,18 +1,14 @@
 package com.api.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 
 @SpringBootApplication
 public class DemoApplication {
-	@Autowired
-	private TokenRelayGatewayFilterFactory filterFactory;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
