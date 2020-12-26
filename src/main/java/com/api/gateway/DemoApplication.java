f20e9310fa50bc205ffd3f8e0dff4cde8da4f31f
zakenn <ennaje.zakari@gmail.com>
2020-12-26 23:59:16 +0100

    add rate limit test

--- src/main/java/com/api/gateway/DemoApplication.java
+++ src/main/java/com/api/gateway/DemoApplication.java
@@ -5,10 +5,13 @@ import com.api.gateway.filters.IpFilter.Config;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @CommonsLog
@@ -16,6 +19,13 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@Bean(name = "userRemoteAddressResolver")
+	public KeyResolver userKeyResolver() {
+		return exchange ->
+			 Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
 	}
 	
 	@Bean
@@ -26,13 +36,21 @@ public class DemoApplication {
 		// todo: add docker-compose to setup dbs and keycloak
 		return builder.routes()
 						.route(p -> p.path("/headers")
-									 .filters(f -> f.addRequestHeader("Hello", "World") )
+									 .filters(f -> f.addRequestHeader("Hello", "World")
+											 .requestRateLimiter()
+													 .rateLimiter( RedisRateLimiter.class,
+															 rl -> rl.setBurstCapacity(1).setReplenishRate(1)
+											// .requestRateLimiter(r ->
+											// 	r.setRateLimiter(new RedisRateLimiter(10, 10) )
+											// )
+									 ).and())
 									 .uri("http://httpbin.org:80"))
 						
 						.route(p -> p.path("/httpbin/cookies")
 										.and()
 										.method(HttpMethod.GET)
-									.filters(f -> f.setPath("/cookies"))
+									.filters(f -> f.setPath("/cookies")
+						)
 									.uri("http://httpbin.org:80"))
 						
 			            .route(p -> p.path("/anything/**")
@@ -41,8 +59,6 @@ public class DemoApplication {
 
 						.build();
 	}
-	
-	
   
   
 }
--- src/main/java/com/api/gateway/controller/GatewayController.java
+++ src/main/java/com/api/gateway/controller/GatewayController.java
@@ -1,15 +1,26 @@
 package com.api.gateway.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.apachecommons.CommonsLog;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.reactive.function.server.ServerRequest;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CommonsLog
 public class GatewayController {
 	  
-	  @RequestMapping("/test")
-	  public Mono<String> test() {
-	      return Mono.just("test");
+	  @GetMapping("/test")
+	  public Mono<?> test(ServerRequest serverRequest) {
+		  return serverRequest.principal().map(p -> {
+			  OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) p;
+			  log.info("Token: " + token.getAuthorizedClientRegistrationId() + "|"
+					  + token.getPrincipal().getAttributes() + "|" + token.getDetails() + "|"+ token.getPrincipal().getClass());
+			  OidcUser oicdUser = (OidcUser)token.getPrincipal();
+			 log.info("OicdUser: "+oicdUser.getClaims()+ "|" + oicdUser.getIdToken().getClaims()+"|"+oicdUser.getAttributes());
+			  return oicdUser;
+		  });
 	  }
 }
