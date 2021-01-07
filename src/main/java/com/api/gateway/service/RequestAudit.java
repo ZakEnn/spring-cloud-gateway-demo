e37a6f613b75929f92bca27784e01ab06a4a5c55
zakenn <ennaje.zakari@gmail.com>
2021-01-07 23:58:51 +0100

    refactoring

--- src/main/java/com/api/gateway/service/RequestAudit.java
+++ src/main/java/com/api/gateway/service/RequestAudit.java
@@ -1,24 +1,21 @@
 package com.api.gateway.service;
 
-import java.util.Objects;
+import com.api.gateway.dto.RequestAuditDto;
+import com.api.gateway.utils.IpUtils;
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ServerWebExchange;
-
-import com.api.gateway.dto.RequestAuditDto;
-
 import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Service
 public class RequestAudit {
-    private static final String X_REAL_IP = "X-Real-IP";
-    private static final String X_FORWARDED_FOR = "X-Forwarded-For";
-    
+
 	 public Mono<RequestAuditDto> createAuditLog(ServerWebExchange exchange, RequestAuditDto entity) {
 	        Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
 	        ServerHttpRequest request = exchange.getRequest();
@@ -26,7 +23,7 @@ public class RequestAudit {
 	        String serviceId = Objects.requireNonNull(route).getId();
 	     
 	        entity.setServiceId(serviceId);
-	        entity.setSourceIp(getRealIp(request));
+	        entity.setSourceIp(IpUtils.getRealIp(request));
 	        entity.setRequestUri(request.getURI().toString());
 	        if (request.getMethod() != null) {
 	            entity.setRequestMethod(request.getMethod().name());
@@ -36,24 +33,5 @@ public class RequestAudit {
 	        entity.setStatusCode(null == response.getStatusCode() ? HttpStatus.FORBIDDEN.value() : response.getStatusCode().value());
 
 	        return Mono.just(entity);
-	    }
-
-	  
-	    private String getRealIp(ServerHttpRequest request) {
-	        HttpHeaders headers = request.getHeaders();
-
-	        String realIp;
-
-	        if (headers.containsKey(X_REAL_IP)) {
-	            realIp = String.valueOf(headers.getFirst(X_REAL_IP));
-	        } else if (headers.containsKey(X_FORWARDED_FOR)) {
-	            realIp = String.valueOf(headers.getFirst(X_FORWARDED_FOR));
-	        } else {
-	            realIp = request.getRemoteAddress() != null ?
-	                    (request.getRemoteAddress().getAddress() != null ?
-	                            request.getRemoteAddress().getAddress().getHostAddress() : "") : "";
-	        }
-
-	        return realIp;
 	    }
 }
--- src/main/java/com/api/gateway/utils/IpUtils.java
+++ src/main/java/com/api/gateway/utils/IpUtils.java
@@ -1,9 +1,32 @@
 package com.api.gateway.utils;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
 import java.util.Arrays;
 import java.util.List;
 
 public final class IpUtils {
 	public static List<String>  WHITE_LISTS = Arrays.asList("localhost", "httpbin.org");
 
+	private static final String X_REAL_IP = "X-Real-IP";
+	private static final String X_FORWARDED_FOR = "X-Forwarded-For";
+
+	public static String getRealIp(ServerHttpRequest request) {
+		HttpHeaders headers = request.getHeaders();
+
+		String realIp;
+
+		if (headers.containsKey(X_REAL_IP)) {
+			realIp = String.valueOf(headers.getFirst(X_REAL_IP));
+		} else if (headers.containsKey(X_FORWARDED_FOR)) {
+			realIp = String.valueOf(headers.getFirst(X_FORWARDED_FOR));
+		} else {
+			realIp = request.getRemoteAddress() != null ?
+					(request.getRemoteAddress().getAddress() != null ?
+							request.getRemoteAddress().getAddress().getHostAddress() : "") : "";
+		}
+
+		return realIp;
+	}
 }
